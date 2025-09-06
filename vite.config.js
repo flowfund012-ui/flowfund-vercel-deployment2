01c8110f12b19077a44866442b60adc8f738d71a
Your Name <youremail@example.com>
2025-09-06 23:20:23 +0200

    update vite config and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react( )],
+  plugins: [
+    react( ),
+    nodePolyfills(),
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
@@ -16,9 +20,4 @@ export default defineConfig({
     },
   },
   base: '/',
-  build: {
-    rollupOptions: {
-      external: ['path', 'buffer'], // Added 'buffer'
-    },
-  },
 })
