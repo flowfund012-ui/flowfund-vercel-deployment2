dc58b57a06b5c22c5d1b830fdd44e67a0525ca6f
flowfund012-ui <flow.fund012@gmail.com>
2026-04-13 21:48:49 +0200

    deploy: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+// Build: v3.1 - WaveFlow logo, Academy, Vault, Language, BankSync
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   experimental: { serverActions: { allowedOrigins: ['*'] } },
+  generateBuildId: async () => 'ff-v3-1-waveflow',
 };
 module.exports = nextConfig;
